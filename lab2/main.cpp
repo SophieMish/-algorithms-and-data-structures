779187879c4aa9421e7d897d4946b88328c8c7e3
Sophia <sophia@mcp2018.local>
2021-02-27 09:14:13 +0300

    lab1 and lab2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -22,24 +22,24 @@ T calc(T num1, T num2, const string &oper) {
 }
 
 int main() {
-//    while (true) {
-    string expr_str;
-    cout << "Enter your expression:";
-    getline(cin, expr_str);
-    vector<string> expr_vec = parseString(expr_str);
+    while (true) {
+        string expr_str;
+        cout << "Enter your expression:";
+        getline(cin, expr_str);
+        vector<string> expr_vec = parseString(expr_str);
 
-    Stack<BinaryNumber<T>> stack;
-    for (auto &el : expr_vec) {
-        if (!isOperation(el)) {
-            stack.push(BinaryNumber<T>(atoi(el.data())));
-        } else {
-            auto num2 = stack.pop();
-            auto num1 = stack.pop();
-            stack.push(calc(num1, num2, el));
+        Stack<BinaryNumber<T>> stack;
+        for (auto &el : expr_vec) {
+            if (!isOperation(el)) {
+                stack.push(BinaryNumber<T>(atoi(el.data())));
+            } else {
+                auto num2 = stack.pop();
+                auto num1 = stack.pop();
+                stack.push(calc(num1, num2, el));
+            }
         }
+        auto res = stack.pop();
+        cout << "Result in Binary: " << res << endl;
+        cout << "Result in Decimal: " << int(res.convert_to_decimal_num()) << endl;
     }
-    auto res = stack.pop();
-    cout << "Result in Binary: " << res << endl;
-    cout << "Result in Decimal: " << int(res.convert_to_decimal_num()) << endl;
 }
-
